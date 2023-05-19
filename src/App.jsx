5ea76e6800a24b3888e96876ec7cc91f2b28baec
nirduk <nirdyk@gmail.com>
2023-05-19 20:25:29 +0300

    nav draft and created jumbotron

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Nav from "./components/nav";
+import Jumbotron from "./components/Jumbotron";
 
 function App() {
 
   return (
     <div className="App">
       <Nav />
+      <Jumbotron />
     </div>
   );
 }
