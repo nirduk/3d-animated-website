714d90f8ac1fde3cbc6d252a12e5668adffd3716
nirduk <nirdyk@gmail.com>
2023-05-23 23:15:56 +0300

    add loader gif animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Jumbotron from "./components/Jumbotron";
 import SoundSection from "./components/SoundSection";
 import DisplaySection from "./components/DisplaySection";
 import WebgiViewer from "./components/WebgiViewer";
+import Loader from "./components/Loader";
 
 function App() {
   const webgiViewerRef = useRef();
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <div className="App">
+      <Loader />
       <div id="content" ref={contentRef}>
         <Nav />
         <Jumbotron />
